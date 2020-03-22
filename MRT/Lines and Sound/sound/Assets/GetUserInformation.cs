59f68135af8e7cd967ec523ff0d1c0445d00fbb3
PinXuan Liu <demi871023@gmail.com>
2020-03-22 21:47:41 +0800

    Demi 2020/03/22

---
+++ MRT/Lines and Sound/sound/Assets/GetUserInformation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GetUserInformation : MonoBehaviour
+{
+
+    public ToggleGroup GenderRadioButton;
+    //public InputField UserName;
+    protected Button StartBtn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenderRadioButton = GetComponent<ToggleGroup>();
+        //UserName = FindObjectOfType<Canvas>().GetComponent<InputField>();
+        StartBtn = GetComponent<Button>();
+        //SelectToggle(1);
+
+        //StartBtn.onClick.AddListener(Click);
+
+        //Debug.Log("Check");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Click()
+    {
+        //UserName.text = "Hi";
+        Debug.Log("Click");
+    }
+
+    /*public void SelectToggle(int id)
+    {
+        var toggles = GenderRadioButton.GetComponentInChildren<Toggles>();
+        toggles[id].isOn = true;
+    }*/
+}
---
+++ MRT/Lines and Sound/sound/Assets/PlaySounds.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaySounds : MonoBehaviour
+{
+
+    int ListIndex = 0;
+    int timer = 0;
+    public float currentTime;
+    public List<string> SongName = new List<string>();
+
+    public Button PlaySoundsButton;
+    public AudioSource Sounds;
+    //public Button PlaySoundsButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("CountTimer");
+
+        PlaySoundsButton.GetComponent<Button>();
+        PlaySoundsButton.onClick.AddListener(PlaySound);
+
+        SongName.Add("Dua Lipa - Physical (feat. Hwa Sa)");
+        SongName.Add("又是吳海英OST");
+
+        Sounds.GetComponent<AudioSource>();
+        //Sounds.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlaySound()
+    {
+        
+        Sounds.clip = Resources.Load<AudioClip>(SongName[ListIndex]);
+        ListIndex++;
+        
+        Sounds.Play();
+    }
+
+    // FixedUpdate執行的間隔時間是相同的
+    /*private void FixedUpdate() {
+        currentTime += Time.fixedDeltaTime;
+        Debug.Log(currentTime);
+    }*/
+
+    // 搭配 StartCoroutine 使用
+    IEnumerator CountTimer()
+    {
+        for(float i = 0 ; i < 5 ; i+=Time.deltaTime)
+        {
+            Debug.Log(i + "second");
+            yield return 0;
+        }
+        Debug.Log("Finish");
+        PlaySound();
+    }
+}
