4d24169f9946d629a5e18f98ce2348e160bd2609
olikepatrick <nina1999210@gmail.com>
2020-04-09 18:25:36 +0800

    Una 4/9

---
+++ MRT/Assets/C#/changeview.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeview : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void moveleft()
+    {
+        transform.Translate(0.3f, 0, 0);
+    }
+
+
+    public void moveright()
+    {
+        transform.Translate(-0.3f, 0, 0);
+    }
+
+    public void gotoBusstation()
+    {
+        SceneManager.LoadScene("bus station");
+    }
+
+    public void gototB1()
+    {
+        SceneManager.LoadScene("B1");
+    }
+
+    public void gotoplatform()
+    {
+        SceneManager.LoadScene("platform");
+    }
+
+     public void gotoclassroom()
+    {
+        SceneManager.LoadScene("classroom");
+    }
+}
---
+++ MRT/Assets/UIController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    // public Text outText;
+     public UnityEngine.UI.Text outText;
+
+// public void OnButtonClicked()
+//     {
+        
+//             outText.text = " clicked, 按鈕1";
+//     }
+ 	public void OnButtonClicked(Button but) {
+        // if (outText != null) {
+        //     outText.text = "<b>Last Interaction:</b>\nUI Button clicked";
+        // }
+
+ 		//outText.text = "<b>Last Interaction:</b>\nUI Button clicked";
+ 		SceneManager.LoadScene("classroom");
+
+
+        /*if("Button".Equals(but.name) )
+        {
+        	 outText.text = "<b>Last Interaction:</b>\nUI Button clicked";
+        }
+
+        if("Button (1)".Equals(but.name))
+        {
+        	SceneManager.LoadScene("selection_all");
+        }*/
+    }
+
+ 	// public void Clicked() {
+  //       if (outText != null) {
+            
+  //       }
+  //   }
+}
+
+//UIController
